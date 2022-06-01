b96fa2ff5a72a352564a52d1815c93887353263c
erickrb83 <80847684+erickrb83@users.noreply.github.com>
2022-06-01 12:10:14 -0400

    Update AppointmentController.java

--- src/Controller/AppointmentController.java
+++ src/Controller/AppointmentController.java
@@ -93,9 +93,9 @@ public class AppointmentController implements Initializable {
      */
     @FXML
     void addAppointmentAction (ActionEvent event) throws SQLException {
-        if(titleText.getText().isEmpty() || locText.getText().isEmpty() || typeText.getText().isEmpty() || descText.getText().isEmpty() ||
-                customerIDCombo.getSelectionModel().isEmpty() || userIDCombo.getSelectionModel().isEmpty() || startTimeCombo.getSelectionModel().isEmpty() ||
-                endTimeCombo.getSelectionModel().isEmpty() || contactIDCombo.getSelectionModel().isEmpty()){
+        if (titleText.getText().isEmpty() || locText.getText().isEmpty() || typeText.getText().isEmpty() || descText.getText().isEmpty() ||
+                customerIDCombo.getPromptText() == null || userIDCombo.getPromptText() == null || startTimeCombo.getPromptText() == null ||
+                endTimeCombo.getPromptText() == null || contactIDCombo.getPromptText() == null){
             Alerts.displayAlert(2);
             return;
         }
